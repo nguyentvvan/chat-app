755afbf0562cddd584c24a3aa392f917cb07059b
Van Nguyen <nguyentvvan@gmail.com>
2023-02-21 20:11:15 +0700

    adding sidebar

---
+++ src/components/ChatRoom/UserInfo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Avatar, Button, Typography } from 'antd';
+
+const WrapperStyle = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: 12px 16px;
+	border-bottom: 1px solid rgba(82, 38, 83);
+
+	.username {
+		color: white;
+		margin-left: 5px;
+	}
+`;
+
+export default function UserInfo() {
+	return (
+		<WrapperStyle>
+			<div>
+				<Avatar>V</Avatar>
+				<Typography.Text className='username'>Van Nguyen</Typography.Text>
+			</div>
+			<Button ghost>Log out</Button>
+		</WrapperStyle>
+	)
+}
